a519864d35863f32e108d40b3c61a9ba0e10b7a4
hamid-ensafi <hamidensafi1380.a@gmail.com>
2024-10-31 05:02:08 +0330

    refactor popup menu

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import "./SignUp.css";
-import person from "../../Assets/person.png";
-import { useState } from "react";
-import { Modal } from "../Modal/Modal";
-import { useNavigate } from "react-router-dom";
+import { openPopup } from "../../slice/popUpslice";
+
+import { useDispatch } from "react-redux";
 const SignUp = () => {
-  const navigate = useNavigate();
-  const handleUserLoginClick = () => {
-    navigate("/modal");
-  };
+  const dispatch = useDispatch();
+
   return (
     <div className="btn-container">
-      <button className="user-btn" onClick={handleUserLoginClick}>
+      <button className="user-btn" onClick={() => dispatch(openPopup())}>
         <span>ورود کابران</span>
       </button>
       <button className="provider-btn">
---
+++ src/slice/popUpslice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    isOpen:false
+}
+const popupSlice = createSlice({
+    name: 'popup',
+    initialState,
+    reducers: {
+        openPopup(state) {
+            state.isOpen=true
+        },
+        closePopup(state) {
+            state.isOpen=false
+        },
+    }
+})
+export default popupSlice.reducer
+
+export const { openPopup, closePopup } = popupSlice.actions
+
+export const getOpen =(state)=>state.ui.isOpen
